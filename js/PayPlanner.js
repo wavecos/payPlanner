f1c7bdce3656a6a69c68a684182230db602191f5
onixdev <onixdev@onixpro.local>
2013-09-02 23:35:05 -0400

    Changes

--- js/PayPlanner.js
+++ js/PayPlanner.js
@@ -2,10 +2,37 @@ var payPlannerAppModule = angular.module('PayPlannerApp', []);
 
 /* Models */
 
+var paymentData = {
+
+    creditAmount: 0, // Monto del Credito
+    timeLimit: 0, // Plazo
+    feesNumber: 0, // Numero de cuotas
+    disbursementDate: new Date(), // Fecha de Desembolso
+    fixedRatePeriod: 0, // Periodo Tasa Fija
+    fixedRate: 0, // Tasa Fija
+    referenceDate: 0, // Tasa de Referencia (TRe)
+    spread: 0, // Spread - Propagar ?
+    periodVariableRate: 0, // Tasa Periodo Variable 
+    lapseBetweenFees: 0, // Lapso Entre Cuotas
+    gracePeriod: 0, // Período de Gracia
+    formCost: 0, // Costo del Formulario
+    desgravamenInsurance: 0, // Seguro de Desgravamen
+
+    prueba: function() {
+        console.log('esto es una salida de consola');
+    }
+};
+
+
 /* Controllers */
 payPlannerAppModule.controller('PaymentsController', function($scope) {
 
-    $scope.fixedFee = {name: "alfredp", test: "prueba"};
+    $scope.payData = paymentData;
+
+    $scope.calculatePayments = function() {
+        console.log('calculamos los pagos');
+    };
+
 
 
 });
